b567b6cd490d1c4e67910b3fb3836f0fe174e962
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-10-04 17:32:34 -0600

    dynamic list for locations on user page stubbed out

--- client/User.js
+++ client/User.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
-const User = (props) => {
-
-  let userView = () => {
-    props.view(false, true)
+export default class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        "location1",
+        "location2",
+        "location3",
+        "location4",
+        "location5"
+      ]
+    }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>User Page</Text>
-      <StatusBar style="auto" />
-      <Button
-        onPress={() => {userView()}}
-        title="back"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-    </View>
-  )
+  userView = () => {
+    this.props.view(false, true)
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Saved Locations</Text>
+        <StatusBar style="auto" />
+        {this.state.list.map((item)=>{
+          return <Button
+            // onPress={()=>{this.locationView()}}
+            title={item}
+            color="#841584"
+          />
+        })}
+        <Button
+          onPress={() => { this.userView() }}
+          title="back"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -38,6 +60,3 @@ const styles = StyleSheet.create({
   }
 })
 
-
-
-export default User;
