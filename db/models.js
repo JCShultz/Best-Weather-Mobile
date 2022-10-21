6d0868ec619ce888bb3f6b6d0a6c38955e89d323
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-10-20 19:05:24 -0600

    removing locations from user lists

--- db/models.js
+++ db/models.js
@@ -12,23 +12,26 @@ exports.returnUser = (req) => {
 exports.addLoc = (req) => {
   return db.UserItem.findOneAndUpdate(
     { name: req.body.name },
-    { $push: {locations: {name: req.body.locations[0].name, lat: req.body.locations[0].lat, long: req.body.locations[0].long} }}
+    { $push: { locations: { name: req.body.locations[0].name, lat: req.body.locations[0].lat, long: req.body.locations[0].long } } }
   )
 }
 
 
 //TODO - remove locations from DB for specific user and matching location name
-exports.deleteLoc = (req) =>{
+exports.deleteLoc = (req) => {
   console.log(req.body.location, req.body.user)
 
+  // return db.UserItem.updateOne(
+  //   { name: req.body.name },
+  //   { $pull: { locations: { location: req.body.location } } }
+  // )
+
   return db.UserItem.find({
-    name: req.body.user
+    "name": req.body.user,
+    "locations.name": req.body.location
   })
-  .then((res)=>{
-    console.log(res);
-  })
-  .catch((err)=>{
-    console.log(err);
+  .then((data)=>{
+    console.log('here: ', data)
   })
 }
 
